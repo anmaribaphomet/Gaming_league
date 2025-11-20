d8d89456f312655d02d73c48e5b82e4ae4c0e5a5
mari <mariamezadiaz2006@gmail.com>
2025-11-20 11:54:12 -0700

    git jdk

--- src/main/java/com/example/Database.java
+++ src/main/java/com/example/Database.java
@@ -9,7 +9,7 @@ public class Database {
     private final String url = "jdbc:postgresql://localhost:5432/Gaming_League";
     private final String user = "developer";
     //private final String password = "2006";
-    private final String password = "Yoongi1910";
+    private final String password = "2006";
 
 
     public Database() {
