02a42dec145559d4faa88d8f544f9c98b109df99
mari <mariamezadiaz2006@gmail.com>
2025-11-29 04:02:29 -0700

    jdbc modificada

--- src/main/java/com/example/JDBCTableAdapter.java
+++ src/main/java/com/example/JDBCTableAdapter.java
@@ -1,8 +1,8 @@
 package com.example;
+
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.table.DefaultTableModel;
 
@@ -15,152 +15,99 @@ public class JDBCTableAdapter extends DefaultTableModel {
     private String sqlColumnNames[];
     private int sqlColumnTypes[];
 
-    public JDBCTableAdapter(ResultSet res, String name) {
+    public JDBCTableAdapter(ResultSet rs, String name) {
         super();
-
-        ResultSet rs = res;
-
+        cargarDatos(rs, true);
         tableName = name;
-
-        int rows = 0;
-        int cols = 0;
-
-        // matriz para cargar los datos de la tabla
-        Object data[][] = null;
-
-        // arreglo con nombres de las columnas de la tabla
-        Object[] columnNames = null;
-
-        try {
-
-            ResultSetMetaData metaData = rs.getMetaData();
-            cols = metaData.getColumnCount();
-
-            columnNames = new Object[cols];
-
-            int c = 0;
-            // Ontener los nombres de las columnas
-            for (int i = 1; i <= cols; i++) {
-                columnNames[c++] = metaData.getColumnLabel(i);
-            }
-
-            // Mover cursor al ultimo renglon
-            rs.last();
-
-            // Cuantos renglones hay?
-            rows = rs.getRow();
-
-            data = new Object[rows][cols];
-
-            // Recorrer cursor al primer renglon
-            rs.beforeFirst();
-
-            int r = 0;
-
-            // Ontener los datos de las columnas
-            while ( rs.next() ) {
-                c = 0;
-                for (int i = 1; i <= cols; i++) {
-                    //System.out.println(rs.getString(i));
-                    data[r][c++] = rs.getString(i);
-                }
-                r++;
-            }
-
-        } catch (SQLException ex) {
-            LOGGER.severe("Error: " + ex.getMessage());
-            LOGGER.severe("Codigo : " + ex.getErrorCode());
-        }
-        this.setDataVector(data, columnNames);
-
     }
 
-    public JDBCTableAdapter(ResultSet res) {
-
+    public JDBCTableAdapter(ResultSet rs) {
         super();
-        ResultSet rs = res;
+        cargarDatos(rs, false);
+    }
+
+    private void cargarDatos(ResultSet rs, boolean ignoreTableMetaName) {
 
         int rows = 0;
         int cols = 0;
 
         Object data[][] = null;
-        Object names[] = null;
+        Object visibleColumnNames[] = null;
 
         try {
+            ResultSetMetaData meta = rs.getMetaData();
 
-            ResultSetMetaData metaData = rs.getMetaData();
+            if (!ignoreTableMetaName) {
+                tableName = meta.getTableName(1);
+            }
 
-            tableName = metaData.getTableName(1);
+            cols = meta.getColumnCount();
 
-            cols = metaData.getColumnCount();
-
-            names = new Object[cols];
+            visibleColumnNames = new Object[cols];
             sqlColumnNames = new String[cols];
             sqlColumnTypes = new int[cols];
 
+
             int c = 0;
             for (int i = 1; i <= cols; i++) {
-                String s = metaData.getCatalogName(i) +", " +
-                        metaData.getColumnLabel(i) + ", " +
-                        metaData.getSchemaName(i);
-                // sqlColumnNames[c] = metaData.getCatalogName(i);
-                sqlColumnNames[c] = metaData.getColumnLabel(i);
-                sqlColumnTypes[c] = metaData.getColumnType(i);
-                names[c++] = metaData.getColumnLabel(i);
-                System.out.println(s);
+
+                String alias = meta.getColumnLabel(i);   // ej: "first_name:Nombre"
+
+                if (alias.contains(":")) {
+                    String[] parts = alias.split(":", 2);
+
+                    sqlColumnNames[c] = parts[0];   // <-- nombre SQL
+                    visibleColumnNames[c] = parts[1]; // <-- nombre visible
+                } else {
+                    sqlColumnNames[c] = alias;
+                    visibleColumnNames[c] = alias;
+                }
+
+                sqlColumnTypes[c] = meta.getColumnType(i);
+                c++;
             }
 
-            // Mover cursor al ultimo renglon
-            rs.last();
 
-            // Cuantos renglones hay?
+            rs.last();
             rows = rs.getRow();
 
             data = new Object[rows][cols];
 
-            // Recorrer cursor al primer renglon
             rs.beforeFirst();
 
+
             int r = 0;
-            while ( rs.next() ) {
+            while (rs.next()) {
                 c = 0;
                 for (int i = 1; i <= cols; i++) {
                     data[r][c++] = rs.getString(i);
                 }
                 r++;
             }
+
         } catch (SQLException ex) {
             LOGGER.severe("Error: " + ex.getMessage());
-            LOGGER.severe("Codigo : " + ex.getErrorCode());
         }
 
-        this.setDataVector(data, names);
+        this.setDataVector(data, visibleColumnNames);
     }
 
-
     public String getSQLColumnName(int i) {
-        String name = null;
-        if( i >= 0 && i < sqlColumnNames.length) {
-            name = sqlColumnNames[i];
+        if (i >= 0 && i < sqlColumnNames.length) {
+            return sqlColumnNames[i];
         }
-        return name;
+        return null;
     }
 
     public int getSQLColumnType(int i) {
-        int dataType = -1;
-        if( i >= 0 && i < sqlColumnTypes.length) {
-            dataType = sqlColumnTypes[i];
+        if (i >= 0 && i < sqlColumnTypes.length) {
+            return sqlColumnTypes[i];
         }
-        return dataType;
+        return -1;
     }
 
     @Override
     public boolean isCellEditable(int row, int column) {
-        if( column == 0 ) {
-            return false;
-        } else {
-            return true;
-        }
+        return false;  // 🔥 AHORA NINGUNA CELDA ES EDITABLE
     }
-
 }
